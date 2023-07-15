fc618fa093210a5e47736f90e2a23c1e6d9e6082
RCLFPLRL <53186400+rcalfplural@users.noreply.github.com>
2023-07-14 20:21:41 -0400

    Updated Build.sh

--- Build.sh
+++ Build.sh
@@ -1,8 +1,12 @@
+echo "[0/4] Initializing building sequence..."
+
 rm -rf Dist
 
 mkdir Dist
 
 cd Dist
+
+echo "[1/4] Creating application directories..."
 
 mkdir bin
 mkdir data
@@ -10,11 +14,15 @@ mkdir out
 
 cd ..
 
+echo "[2/4] Loading necessary data for the run..."
+
 cp -r node_modules Dist/data
 cp -r src Dist/data
 cp package.json Dist/data
 
+echo "[3/4] Creating binaries..."
+
 cp node.exe Dist/bin
 cp Musiker.bat Dist
 
-echo Sex is done here.
+echo "[4/4] Building completed!"
